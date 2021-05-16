c2e00fab4c46f8573209d28b778fcc91e84c3289
Edi <Edisontianyi.wang@gmail.com>
2021-05-16 20:13:02 +0200

    16.05.

--- src/spiel/GameBoard.java
+++ src/spiel/GameBoard.java
@@ -1,4 +1,13 @@
 package spiel;
 
+import java.util.*;
+
 public class GameBoard {
+    Field[] gameBoard = new Field[40];
+
+    public GameBoard() {
+        for (int i = 0; i < gameBoard.length; i++) {
+            
+        }
+    }
 }
