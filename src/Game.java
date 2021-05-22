3cb52b988bc50c30592e715d9f7a34f276a624e3
Edi <Edisontianyi.wang@gmail.com>
2021-05-22 22:52:18 +0200

    22.05.

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+import spiel.Figure;
+
+public class Game {
+    public class Player {
+        public int playersOnTarget;
+        public Figure[] playerFigures = new Figure[4];
+        public int color;
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,7 @@
+import spiel.Figure;
+
+public class Player {
+    public int playersOnTarget;
+    public Figure[] playerFigures = new Figure[4];
+    public int color;
+}
